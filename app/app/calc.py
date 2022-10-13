6d149d1a6ce4772c3f37651ceacdd6d70f0b8e1f
Aleksandar Simonovski <simonovski@gmail.com>
2022-10-13 08:48:03 +0200

    Add PostgreSQL

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y and return results."""
+    return x + y    
+
+def subtract(x, y):
+    """Substact x from y and return results."""
+    return y - x
