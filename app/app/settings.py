2e7f22c17b59b0f602ca87beb820544264a5009c
Aleksandar Simonovski <simonovski@gmail.com>
2022-11-04 18:40:54 +0100

    Update django settings fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool.(int(os.environ.get(DEBUG, 0)))
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
     filter(
         None,
-        os.environ.get('ALLOWED_HOSTS, '').split(','),
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
     )
 )
 
